e8787c8110bd7abc7104718d1f1cf784d268c39a
m3vance <m3vance@gmail.com>
2023-06-16 09:26:12 -0700

    completed signup and signin

---
+++ src/__test__/SignIn.test.js
@@ -0,0 +1,19 @@
+import { render, screen } from "@testing-library/react";
+import { BrowserRouter } from "react-router-dom";
+import Home from "../pages/Home.js";
+
+describe("<SignIn />", () => {
+it("renders without crashing", () => {
+    render(
+    <BrowserRouter>
+        <Home />
+    </BrowserRouter>
+    );
+
+    const component = document.querySelector('#SignIn')
+
+    expect(component).toBeInTheDocument
+
+    });
+
+}); 
---
+++ src/__test__/SignUp.test.js
@@ -0,0 +1,19 @@
+import { render, screen } from "@testing-library/react";
+import { BrowserRouter } from "react-router-dom";
+import Home from "../pages/Home.js";
+
+describe("<SignUp />", () => {
+it("renders without crashing", () => {
+    render(
+    <BrowserRouter>
+        <Home />
+    </BrowserRouter>
+    );
+
+    const component = document.querySelector('#SignUp')
+
+    expect(component).toBeInTheDocument
+
+    });
+
+}); 
